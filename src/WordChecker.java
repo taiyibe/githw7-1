fbbe1ccd554e8b27a002f81e0513d30b2c816591
Viktoria Kravchenko <taiyibe@yandex.ru>
2023-03-15 17:03:36 +0300

    Оптимизация кода в WordChecker.java

--- src/WordChecker.java
+++ src/WordChecker.java
@@ -1,16 +1,13 @@
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class WordChecker {
-    private String Text;
+
     private Set<String> words;
 
     public WordChecker(String txt) {
-        Text = txt;
-        words = new HashSet<>();
-        for (String word : Text.split("\\P{IsAlphabetic}+")) {
-            words.add(word);
-        }
+        words = new HashSet<>(List.of(txt.split("\\P{IsAlphabetic}+")));
     }
 
     public boolean hasWord(String wrd) {
